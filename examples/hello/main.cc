ae9c34b7eed8ce5f114ec3927968c92dfa5842e6
Silas Sewell <silas@sewell.ch>
2010-06-08 09:21:05 -0600

    Style tweak

--- examples/hello/main.cc
+++ examples/hello/main.cc
@@ -4,7 +4,7 @@
 #include <talk/xmpp/xmppclientsettings.h>
 #include "xmppthread.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char* argv[]) {
 
   talk_base::LogMessage::LogToDebug(talk_base::LS_SENSITIVE);
 
