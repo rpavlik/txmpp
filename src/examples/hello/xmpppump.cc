97625c691726ff0506f018d7daef31144072c030
Silas Sewell <silas@sewell.ch>
2010-06-12 19:24:22 -0600

    Switch to case and print error

--- src/examples/hello/xmpppump.cc
+++ src/examples/hello/xmpppump.cc
@@ -27,6 +27,8 @@
 
 #include "xmpppump.h"
 
+#include <iostream>
+
 #include "../../logging.h"
 #include "../../prexmppauthimpl.h"
 #include "xmpptasks.h"
@@ -61,21 +63,31 @@ void XmppPump::DoDisconnect() {
 void XmppPump::OnStateChange(txmpp::XmppEngine::State state) {
   if (state_ == state)
     return;
-  if (state == txmpp::XmppEngine::STATE_OPEN) {
-    // task_message, task_precence and task_iq are deleted by client_
-    //
-    // This accepts <message/> stanzas and prints the sender and message
-    // to stdout
-    XmppTaskMessage *task_message = new XmppTaskMessage(client_);
-    task_message->Start();
-    // This accepts <presence/> stanzas and prints whom they're from
-    // to stdout
-    XmppTaskPresence *task_presence = new XmppTaskPresence(client_);
-    task_presence->Start();
-    // This sends a privacy list request on Start and handles only its
-    // response
-    XmppTaskIq *task_iq = new XmppTaskIq(client_);
-    task_iq->Start();
+  std::cout << "STATE: " << state_ << std::endl;
+  switch(state) {
+    case txmpp::XmppEngine::STATE_OPEN: {
+      // task_message, task_precence and task_iq are deleted by client_
+      //
+      // This accepts <message/> stanzas and prints the sender and message
+      // to stdout
+      XmppTaskMessage *task_message = new XmppTaskMessage(client_);
+      task_message->Start();
+      // This accepts <presence/> stanzas and prints whom they're from
+      // to stdout
+      XmppTaskPresence *task_presence = new XmppTaskPresence(client_);
+      task_presence->Start();
+      // This sends a privacy list request on Start and handles only its
+      // response
+      XmppTaskIq *task_iq = new XmppTaskIq(client_);
+      task_iq->Start();
+      }
+      break;
+    case txmpp::XmppEngine::STATE_START:
+    case txmpp::XmppEngine::STATE_OPENING:
+      break;
+    case txmpp::XmppEngine::STATE_CLOSED:
+      std::cout << "Error: " << client_->GetError(NULL) << std::endl;
+      break;
   }
   state_ = state;
   if (notify_ != NULL)
